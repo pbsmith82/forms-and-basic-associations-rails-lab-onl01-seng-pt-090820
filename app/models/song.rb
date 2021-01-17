cfdf2babcd50fa3faa12351f33e21a7ce747bc69
pbsmith82 <pbsmith82@gmail.com>
2021-01-17 10:50:26 -0500

    commit tree d671801f2b9d3ab4db410597b9e794d395cb94b6

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,41 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist 
+  belongs_to :genre
+  has_many :notes
+
+
+  def genre_id=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_id
+     self.genre ? self.genre.name : nil
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+     self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      if note.strip != ""
+        content = self.notes.build(content: note)
+        content.save
+      end
+    end
+  end
+
+  #reader for note contents
+  def note_contents
+    self.notes.collect {|note| note.content }
+  end
+
+
+
+
 end
